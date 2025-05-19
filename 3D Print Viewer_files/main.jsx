348751b1abaa9acd4c9e31c12c7b7c21304b0663
samarth-uttam <samarth.uttam@icloud.com>
2025-05-19 13:33:41 +0800

    Updates

---
+++ 3D Print Viewer_files/main.jsx
@@ -0,0 +1,19 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=04786f56"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_react from "/node_modules/.vite/deps/react.js?v=04786f56"; const StrictMode = __vite__cjsImport1_react["StrictMode"];
+import __vite__cjsImport2_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=04786f56"; const createRoot = __vite__cjsImport2_reactDom_client["createRoot"];
+import "/src/Styles/index.css";
+import "/src/Styles/main.css";
+import App from "/src/App.jsx?t=1747216008217";
+createRoot(document.getElementById("root")).render(
+  /* @__PURE__ */ jsxDEV(StrictMode, { children: /* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
+    fileName: "/Users/sam/Desktop/DShape Slicer/Dshape Slicer/src/main.jsx",
+    lineNumber: 10,
+    columnNumber: 5
+  }, this) }, void 0, false, {
+    fileName: "/Users/sam/Desktop/DShape Slicer/Dshape Slicer/src/main.jsx",
+    lineNumber: 9,
+    columnNumber: 3
+  }, this)
+);
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IkFBU0k7QUFUSixTQUFTQSxrQkFBa0I7QUFDM0IsU0FBU0Msa0JBQWtCO0FBQzNCLE9BQU87QUFDUCxPQUFPO0FBQ1AsT0FBT0MsU0FBUztBQUdoQkQsV0FBV0UsU0FBU0MsZUFBZSxNQUFNLENBQUMsRUFBRUM7QUFBQUEsRUFDMUMsdUJBQUMsY0FDQyxpQ0FBQyxTQUFEO0FBQUE7QUFBQTtBQUFBO0FBQUEsU0FBSSxLQUROO0FBQUE7QUFBQTtBQUFBO0FBQUEsU0FFQTtBQUNGIiwibmFtZXMiOlsiU3RyaWN0TW9kZSIsImNyZWF0ZVJvb3QiLCJBcHAiLCJkb2N1bWVudCIsImdldEVsZW1lbnRCeUlkIiwicmVuZGVyIl0sImlnbm9yZUxpc3QiOltdLCJzb3VyY2VzIjpbIm1haW4uanN4Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IFN0cmljdE1vZGUgfSBmcm9tICdyZWFjdCdcbmltcG9ydCB7IGNyZWF0ZVJvb3QgfSBmcm9tICdyZWFjdC1kb20vY2xpZW50J1xuaW1wb3J0ICcuL1N0eWxlcy9pbmRleC5jc3MnXG5pbXBvcnQgJy4vU3R5bGVzL21haW4uY3NzJ1xuaW1wb3J0IEFwcCBmcm9tICcuL0FwcC5qc3gnXG5cblxuY3JlYXRlUm9vdChkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgncm9vdCcpKS5yZW5kZXIoXG4gIDxTdHJpY3RNb2RlPlxuICAgIDxBcHAgLz5cbiAgPC9TdHJpY3RNb2RlPixcbilcbiJdLCJmaWxlIjoiL1VzZXJzL3NhbS9EZXNrdG9wL0RTaGFwZSBTbGljZXIvRHNoYXBlIFNsaWNlci9zcmMvbWFpbi5qc3gifQ==
